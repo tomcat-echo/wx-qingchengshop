1133b16718f945ebd6f870074e49cbf2af6d5e10
echocat <2332046984@qq.com>
2023-02-01 15:53:51 +0800

    首页效果

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 导入网络请求的包
+import {
+	$http
+} from '@escook/request-miniprogram'
 
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 请求前展示loading
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 请求后关闭loading
+$http.afterRequest = function() {
+	uni.$showToast('数据请求成功')
+	uni.hideLoading()
+}
+
+// 封装请求成功/失败提示
+uni.$showToast = function(title = '数据请求失败', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,45 @@
 <template>
 	<view>
-		home
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true" aria-role="">
+			<swiper-item v-for="(item,index) in swiperList" :key="index" :aria-label="`轮播图${index+1}`">
+				<navigator class="swiper-item" :url="`/subpackage/goods_detail/goods_detail?goods_id=`+ item.goods_id">
+					<image :src="item.image_src" />
+				</navigator>
+			</swiper-item>
+
+		</swiper>
+
+		<view class="nav-list">
+			<view class="nav-item" @click="navClickHandler(item)" v-for="(item, i) in navigatorList" :key="i">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
+
+		<view class="floor-list">
+			<view class="floor-item" v-for="(item,index) in floorList" :key="index">
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<view class="floor-imglist">
+					<navigator class="left-img-box" :url="item.product_list[0].url">
+
+						<image :src="item.product_list[0].image_src"
+							:style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+
+					</navigator>
+					<view class="right-img-box">
+						<navigator class="right-item" v-for="(item2,index2) in item.product_list" :key="index" :url="item2.url">
+							<image :src="item2.image_src" :style="{width: item2.image_width + 'rpx'}" mode="widthFix"
+								v-if="index2!= 0">
+							</image>
+						</navigator>
+					</view>
+
+				</view>
+			</view>
+
+
+
+		</view>
+
 	</view>
 </template>
 
@@ -8,12 +47,95 @@
 	export default {
 		data() {
 			return {
-
+				// 1. 轮播图的数据列表，默认为空数组
+				swiperList: [],
+				navigatorList: [], //导航列表
+				floorList: [] //楼层
 			};
+		},
+		onLoad() {
+			// 2. 在小程序页面刚加载的时候，调用获取轮播图数据的方法
+			this.getSwiperList()
+			this.getNavigatorList()
+			this.getFloorList()
+		},
+		methods: {
+			// 3. 获取轮播图数据的方法
+			async getSwiperList() {
+				// 3.1 发起请求 
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				this.swiperList = res.message
+			},
+			async getNavigatorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				this.navigatorList = res.message
+			},
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						// 自定义一个属性url,同时拼接通过split获得后半部分
+						prod.url =  '/subpackage/goods_list/goods_list?' + prod.navigator_url.split("?")[1]
+					})
+				})
+				this.floorList = res.message
+			},
+
+			navClickHandler(item) {
+				if (item.name === '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		image {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	.floor-title {
+		height: 60rpx;
+		width: 100%;
+		display: flex;
+	}
+
+	.floor-imglist {
+		display: flex;
+		padding-left: 10rpx;
+
+		.right-img-box {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-around;
+		}
+	}
 </style>
